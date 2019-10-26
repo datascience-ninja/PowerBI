491dcb818f13cb84fd6217be1172b337269a15fc
ian santillan <ievsantillan@gmail.com>
2019-10-26 11:46:38 -0400

    Replace Text.Replace with Replacer.ReplaceValue

--- PowerQuery/BulkTextReplaceValue/BulkTextReplaceValue.m
+++ PowerQuery/BulkTextReplaceValue/BulkTextReplaceValue.m
@@ -2,9 +2,9 @@
 let
     maxIterations = Table.RowCount(ConversionTable) ,
  Iterations = List.Generate( () =>
-              [Result = Text.Replace(x, ConversionTable[OldText]{0}, ConversionTable[NewText]{0}), Counter = 0],
+              [Result = Replacer.ReplaceValue(x, ConversionTable[OldText]{0}, ConversionTable[NewText]{0}), Counter = 0],
                     each [Counter] < maxIterations,
-                    each [Result = Text.Replace([Result], ConversionTable[OldText]{Counter}, ConversionTable[NewText]{Counter}),
+                    each [Result = Replacer.ReplaceValue([Result], ConversionTable[OldText]{Counter}, ConversionTable[NewText]{Counter}),
                 Counter = [Counter] +1], each [Result]),
   output = Iterations{maxIterations-1}
 in
